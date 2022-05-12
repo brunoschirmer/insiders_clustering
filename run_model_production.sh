32dd530778bc8784ff66c7d054dfd73069de4f94
brunoschirmer <bruno.schirmer@hotmail.com>
2022-05-12 18:21:17 -0300

    deployment 2

--- run_model_production.sh
+++ run_model_production.sh
@@ -2,7 +2,7 @@
 data=$(date +'%Y-%m-%dT%H:%M:%S')
 
 #_ path 
-path='/home/ubuntu/pa005_insiders_clustering/insiders_clustering'
+path='/home/ubuntu/insiders'
 path_to_envs='/home/ubuntu/.local/bin/'
 
 $path_to_envs/papermill $path/src/models/c10-bs-deploy.ipynb $path/reports/c109-bs-deploy-$data.ipynb
