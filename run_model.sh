91aad552d00314aa65957506ac31c7503d5959f2
brunoschirmer <bruno.schirmer@hotmail.com>
2022-05-16 19:56:23 -0300

    clean production notebook

--- run_model.sh
+++ run_model.sh
@@ -1,8 +1,8 @@
 # variable
 data=$(date +'%Y-%m-%dT%H:%M:%S')
 
-# path 
+# path
 path='/Users/brunoschirmer/repos/insiders'
-path_to_envs='/Users/brunoschirmer/.pyenv/versions/3.8.0/envs/pa005insidersclustering/bin'
+path_to_envs='/Users/brunoschirmer/.pyenv/versions/3.9.12/envs/insiders/bin'
 
 $path_to_envs/papermill $path/src/models/c10-bs-deploy.ipynb $path/reports/c10-bs-deploy_$data.ipynb
